2caef0518fc8b070de7e1cf19e100df214891f44
shinter61 <shinter61@gmail.com>
2021-06-25 10:39:20 +0900

    Initial Commit

---
+++ SlokinTimer/SlokinTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlokinTimerApp.swift
+//  SlokinTimer
+//
+//  Created by 松本真太朗 on 2021/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SlokinTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
