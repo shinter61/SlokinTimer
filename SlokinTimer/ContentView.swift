24dfe93d256c3961214927ae315f83b7328b4032
shinter61 <shinter61@gmail.com>
2021-06-27 00:45:28 +0900

    Enable set start time

--- SlokinTimer/ContentView.swift
+++ SlokinTimer/ContentView.swift
@@ -14,8 +14,21 @@ struct ContentView: View {
         VStack {
             Text(String(timeHolder.current))
                 .padding()
+            Button(action: {
+                timeHolder.set()
+            }) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.yellow)
+                        .frame(width: 180, height: 50)
+                    Text("スロ禁開始")
+                        .foregroundColor(.white)
+                        .font(.custom("Shippori Mincho", size: 24))
+                        .fontWeight(.bold)
+                }
+            }
         }
-        .onAppear { timeHolder.start() }
+//        .onAppear { timeHolder.start() }
     }
 }
 
--- SlokinTimer/TimeHolder.swift
+++ SlokinTimer/TimeHolder.swift
@@ -11,16 +11,35 @@ import SwiftUI
 final class TimeHolder: ObservableObject {
     @Published var timer: Timer!
     @Published var current: String = ""
-    
+    @Published var startTime: String = ""
+
     func start() {
         self.timer?.invalidate()
         self.timer = Timer.scheduledTimer(withTimeInterval: 1/60, repeats: true) { _ in
-            let currentTime: Date = Date()
+            var calendar = Calendar.current
+            calendar.locale = Locale(identifier: "ja_JP")
+            
             let formatter = DateFormatter()
             formatter.locale = Locale(identifier: "ja_JP")
-            formatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
             formatter.dateFormat = "yy/MM/dd HH:mm:ss"
-            self.current = formatter.string(from: currentTime)
+            let currentTime: Date = Date()
+            let startTimeDate: Date = formatter.date(from: self.startTime)!
+            
+            let diffFormatter = DateComponentsFormatter()
+            diffFormatter.calendar = calendar
+            diffFormatter.unitsStyle = .full
+
+            let timeDiff = calendar.dateComponents([.day, .hour, .minute, .second], from: startTimeDate, to: currentTime)
+            self.current = diffFormatter.string(from: timeDiff)!
         }
+    }
+    
+    func set() {
+        let currentTime: Date = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "yy/MM/dd HH:mm:ss"
+        self.startTime = formatter.string(from: currentTime)
+        self.start()
     }
 }
